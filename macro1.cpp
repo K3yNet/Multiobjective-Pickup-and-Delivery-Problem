47ffb49066674a8f0c3dba1247d7561590aff58f
K3yNet <53867555+K3yNet@users.noreply.github.com>
2023-02-22 15:09:36 -0300

    Update macro1.cpp

--- macro1.cpp
+++ macro1.cpp
@@ -16,38 +16,95 @@ Rafael Brunini : 202120488
 
 using namespace std;
 
+int ref = 1;
+
+vector<int> geraRotas(Instancia inst, Pontos *totalPontos, int index, Truck caminhao){
+    vector<int> rota;
+    rota.push_back(0);
+    rota.push_back(index);
+    rota.push_back(totalPontos[index].pEntrega);
+    rota.push_back(0);
+    ref++;
+    for (int i = 0; i < rota.size(); i++){
+        caminhao.tempoGasto += totalPontos[rota[i]].duracaoServico;
+        caminhao.ocupacao += totalPontos[rota[i]].demanda;
+    }
+    return rota;
+}
+
 int main(){
 
-    Truck caminhao;
-
-    string posicaoAtualArquivo = "poa-n100-6.txt";
+    // nyc-n100-4.txt
+    // poa-n100-6.txt
+    // bar-n100-1.txt
+    // poa-n5000-3.txt
+    string posicaoAtualArquivo = "poa-n100-6.txt ";
 
     Instancia instancia;
 
     LerInstancia(posicaoAtualArquivo,instancia);
 
+    vector<Truck> caminhoes;
 
     Pontos *totalPontos = new Pontos[instancia.tamanho_instancia];
     int **MA = new int*[instancia.tamanho_instancia];
 
     leituraGrafo(posicaoAtualArquivo, instancia.tamanho_instancia, MA, totalPontos);
-    caminhao.rota.push_back(0);
-    caminhao.rota.push_back((instancia.tamanho_instancia - 1) / 2);
-    caminhao.rota.push_back(instancia.tamanho_instancia - 1);
-    vector<Truck> caminhoes;
-    caminhoes.push_back(caminhao);
-    
-    Pontos *coletas = new Pontos[(instancia.tamanho_instancia -1 )/2];
-    for(int i = 0; i < (instancia.tamanho_instancia -1 )/2; i++)
+
+    Pontos *coletas = new Pontos[(instancia.tamanho_instancia - 1 ) / 2];
+    for(int i = 0; i < (instancia.tamanho_instancia - 1 ) / 2; i++){
         coletas[i] = totalPontos[i];
-
-    quickSort(coletas,0,(instancia.tamanho_instancia-1)/2);
-
-    for(int i = 0; i < (instancia.tamanho_instancia -1 )/2;i++)
         cout << coletas[i].id << " ";
+    }
+    cout << endl;
 
+    selectionSort(coletas, instancia);
+
+    for(int i = 0; i < (instancia.tamanho_instancia - 1 ) / 2; i++)
+        cout << coletas[i].id << " ";
+    cout << endl;
+
+    vector<int> rota;
+    int finish = 0;
     
-    mais_proximo(caminhoes[0].rota, MA, instancia, totalPontos, caminhoes);
+    while (finish != 1){
+        Truck caminhao;
+        int index = coletas[0 + ref].id;
+
+        label1:
+        if (!coletas[0 + ref].visitado)
+            rota = geraRotas(instancia, totalPontos, index, caminhao);
+        else{
+            ref++;
+            goto label1;
+        }
+        caminhao.rota = mais_proximo(caminhao, rota, MA, instancia, totalPontos, finish);
+        // caminhao.tempoGasto += custo_total(caminhao.rota, MA);
+        cout << "Melhor rota: " << endl;
+        for (int i = 0; i < caminhao.rota.size(); i++){
+            cout << caminhao.rota[i] << " ";
+        }
+        cout << endl;
+        caminhoes.push_back(caminhao);
+    }
+
+    int custo = INF;
+
+    for (int i = 0; i < caminhoes.size(); i++){
+        if (verificaRestricao(totalPontos, instancia, caminhoes[i].rota.insert(caminhoes[i].rota.begin + 1, caminhoes[i + 1].rota))){
+            caminhoes[i].rota.insert(caminhoes[i].rota.begin + 1, caminhoes[i + 1].rota);
+            caminhoes[i + 1]
+        }
+        
+    }
+    
+
+    cout << "Total de caminhoes: " << caminhoes.size() << endl;
+    int custo = 0;
+    for (int i = 0; i < caminhoes.size(); i++){
+        custo += custo_total(caminhoes[i].rota, MA);
+    }
+    cout << "Custo total: " << custo << endl;
 
     return 0;
 }
